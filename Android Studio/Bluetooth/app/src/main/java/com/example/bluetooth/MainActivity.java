2b1ccf60975261e4920032483a33caa7496d734f
Sangwoo Suk <56511253+sangwoo24@users.noreply.github.com>
2020-05-02 19:36:43 +0900

    Bluetooth 연결 및 페어링 버튼생성

--- Android Studio/Bluetooth/app/src/main/java/com/example/bluetooth/MainActivity.java
+++ Android Studio/Bluetooth/app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -38,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     private int readBufferPosition; // 버퍼 내 문자 저장 위치
     private int pariedDeviceCount;
 
+    private Button btnBtOn;
+    private Button btnBtOff;
+    private Button btnConnect;
     private TextView textViewReceive; // 수신 된 데이터를 표시하기 위한 텍스트 뷰
 
 
@@ -47,13 +50,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         // 각 컨테이너들의 id를 매인 xml과 맞춰준다.
         textViewReceive = (TextView) findViewById(R.id.textView_receive);
 
+        btnBtOn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bluetoothOn();
+            }
+        });
+        btnBtOff.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bluetoothOff();
+            }
+        });
+        btnConnect.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectBluetoothDevice();
+            }
+        });
 
+    }
 
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
+    void bluetoothOn() {
         if (bluetoothAdapter == null)
         { // 디바이스가 블루투스를 지원하지 않을 때
             Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_LONG).show();
@@ -61,17 +84,29 @@ public class MainActivity extends AppCompatActivity {
         else { // 디바이스가 블루투스를 지원 할 때
             if (bluetoothAdapter.isEnabled())
             { // 블루투스가 활성화 상태 (기기에 블루투스가 켜져있음)
-                selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+                Toast.makeText(getApplicationContext(), "블루투스가 이미 활성화 되어 있습니다.", Toast.LENGTH_LONG).show();
+                //selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
             }
             else { // 블루투스가 비 활성화 상태 (기기에 블루투스가 꺼져있음)
                 // 블루투스를 활성화 하기 위한 다이얼로그 출력
+                Toast.makeText(getApplicationContext(), "블루투스가 활성화 되어 있지 않습니다.", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 // 선택한 값이 onActivityResult 함수에서 콜백된다.
                 startActivityForResult(intent, REQUEST_ENABLE_BT);
             }
         }
     }
-    @Override
+
+    void bluetoothOff() {
+        if (bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.disable();
+            Toast.makeText(getApplicationContext(), "블루투스가 비활성화 되었습니다.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "블루투스가 이미 비활성화 되어 있습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -79,14 +114,17 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case REQUEST_ENABLE_BT:
                 if (requestCode == RESULT_OK) { // '사용'을 눌렀을 때
-                    selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+                    //selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+                    Toast.makeText(getApplicationContext(), "블루투스 활성화", Toast.LENGTH_LONG).show();
                 } else { // '취소'를 눌렀을 때
                     // 여기에 처리 할 코드를 작성하세요.
-                    Toast.makeText(getApplicationContext(), "취소", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "블루투스 비활성화", Toast.LENGTH_LONG).show();
                 }
                 break;
         }
     }
+
+
 
     public void selectBluetoothDevice() {
         // 이미 페어링 되어있는 블루투스 기기를 찾습니다.
@@ -162,15 +200,11 @@ public class MainActivity extends AppCompatActivity {
         final Handler handler = new Handler();
 
         // 데이터를 수신하기 위한 버퍼를 생성
-
         readBufferPosition = 0;
-
         readBuffer = new byte[1024];
 
 
-
         // 데이터를 수신하기 위한 쓰레드 생성
-
         workerThread = new Thread(new Runnable() {
             @Override
             public void run() {
