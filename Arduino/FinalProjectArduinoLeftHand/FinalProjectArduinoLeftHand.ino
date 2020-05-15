fe99c48ef43d87272522ae369e0c132164741f32
Sangwoo Suk <56511253+sangwoo24@users.noreply.github.com>
2020-05-16 00:14:33 +0900

    아두이노 오른손, 왼손코드 추가

---
+++ Arduino/FinalProjectArduinoLeftHand/FinalProjectArduinoLeftHand.ino
@@ -0,0 +1,40 @@
+#include <SoftwareSerial.h>
+#define SBUF_SIZE 1024
+SoftwareSerial bluetooth(2, 3);
+
+float ebimuTest[6] = { 0.00, 10.00, 50.00, 100.00, 150.00, 200.00}; 
+char str[6][20];
+
+void setup()
+{
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+}
+
+void loop() {
+   if(bluetooth.available())
+   {
+     Serial.println(bluetooth.read());
+   }
+
+   for(int i = 0; i < 6; i++)
+   {
+      Serial.print(ebimuTest[i]);
+      dtostrf(ebimuTest[i],7,2,str[i]);
+      bluetooth.write(str[i]);
+      if(i <= 4)
+      {
+        bluetooth.write(",");
+      }
+      else if(i == 5)
+      {
+        bluetooth.write('\n');
+        Serial.println();
+      }
+   }
+   for(int i = 0; i < 6; i++)
+   {
+     ebimuTest[i] += 1.00;
+   }
+   delay(50);
+}
